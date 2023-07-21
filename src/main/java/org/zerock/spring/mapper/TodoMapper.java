9e304f75d8ce9b48174dcb3a6517c0cf69eb1a34
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-21 18:45:00 +0900

    Todo 기능 개발(CRUD) & 페이지 처리를 위한 객체 생성 및 테스트 코드 작성

--- src/main/java/org/zerock/spring/mapper/TodoMapper.java
+++ src/main/java/org/zerock/spring/mapper/TodoMapper.java
@@ -1,10 +1,27 @@
 package org.zerock.spring.mapper;
 
 import org.zerock.spring.domain.TodoVO;
+import org.zerock.spring.dto.PageRequestDTO;
+
+import java.util.List;
 
 public interface TodoMapper {
 
     String getTime();
 
     void insert(TodoVO todoVO);
+
+    List<TodoVO> selectAll();
+
+    TodoVO selectOne(Long tno);
+
+    void delete(Long tno);
+
+    void update(TodoVO todoVO);
+
+    // TodoMapper 목록 처리
+    List<TodoVO> selectList(PageRequestDTO pageRequestDTO);
+
+    // 전체 데이터 수 (페이지 번호 구성)
+    int getCount(PageRequestDTO pageRequestDTO);
 }
